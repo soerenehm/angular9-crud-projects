ea24be815ee3e02c40bb7415b090af5a8a554063
soerenehm <info@soeren-ehm.de>
2020-07-21 15:27:13 +0200

    Router links dependant from authentification

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,6 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AuthService} from '../../auth/auth.service';
+import {RouterTestingModule} from '@angular/router/testing';
+
+import {HeaderComponent} from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +12,10 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [AuthService]
+    });
   }));
 
   beforeEach(() => {
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  userSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    console.log('HeaderComponent constructor');
+  }
 
   ngOnInit() {
+    this.userSubscription = this.authService.userChanged.subscribe((user) => {
+      this.isAuthenticated = user !== null;
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 
 }
